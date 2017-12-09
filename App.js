0c93c486a16a3304c7ece78800007204116421bb
Kenia Valladarez <keniavalladarez@gmail.com>
2017-12-08 22:28:43 -0500

    Adds the styling for the input box.

--- App.js
+++ App.js
@@ -45,4 +45,14 @@ const styles = StyleSheet.create({
   smallText: {
     fontSize: 18,
   },
+  textInput: {
+    backgroundColor: '#666',
+    color: 'white',
+    height: 40,
+    width: 300,
+    marginTop: 20,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    alignSelf: 'center',
+  }
 });
