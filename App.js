d83e4e108d0b353f54302db697e5c60c9f8f3a12
Kenia Valladarez <keniavalladarez@gmail.com>
2017-12-08 22:27:02 -0500

    Adds an text input to the component.

--- App.js
+++ App.js
@@ -15,6 +15,14 @@ export default class App extends React.Component {
         <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
         <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
         <Text style={[styles.largeText, styles.textStyle]}>24</Text>
+
+        <TextInput
+          autoCorrect={false}
+          placeholder="Search any city"
+          placeholderTextColor="white"
+          style={styles.textInput}
+          clearButtonMode="always"
+        />
       </View>
     );
   }
