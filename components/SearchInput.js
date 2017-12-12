443815e54298914f2722ba3cc3fea701111b2640
Kenia Valladarez <keniavalladarez@gmail.com>
2017-12-11 22:02:27 -0500

    Adds the function handleChangeText and uses property initializer.

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -6,6 +6,10 @@ import {
 } from "react-native";
 
 export default class SearchInput extends React.Component {
+  handleChangeText = (newLocation) => {
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -16,6 +20,7 @@ export default class SearchInput extends React.Component {
           underlineColorAndroid='transparent'
           style={styles.textInput}
           clearButtonMode='always'
+          onChangeText={this.handleChangeText}
         />
       </View>
     );
